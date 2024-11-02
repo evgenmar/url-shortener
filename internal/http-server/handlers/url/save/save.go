9cd0f17088628a9524ad55fabaee34d4819f15b5
Evgeny Martynov <evgemar@mail.ru>
2024-11-02 13:01:11 +0300

    Added test for handler "save"

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -24,6 +24,7 @@ type Response struct {
 	Alias string `json:"alias,omitempty"`
 }
 
+//go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name=URLSaver
 type URLSaver interface {
 	SaveURL(urlToSave, alias string) (int64, error)
 }
